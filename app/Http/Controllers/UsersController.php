8a8e344e0c6ed723b7c2f052424fd9bcc2bae306
frank <vagrant@homestead>
2020-07-22 10:38:06 +0000

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,10 +10,21 @@ class UsersController extends Controller
     public function create()
     {
         return view('users.create');
+        // 为什么return一个view函数就可以了？
     }
 
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
